115d9585ab0e26cc7389af2ef07bf0cd5e6d6e64
sideonkeibulllll <kalamaqaq@qq.com>
2026-01-02 16:47:17 +0800

    修复静态文件输出目录为 public 以适配 Vercel

--- src/build-static.js
+++ src/build-static.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const outputDir = path.join(__dirname, '../static');
+const outputDir = path.join(__dirname, '../public');
 
 class StaticPageGenerator {
   constructor() {
